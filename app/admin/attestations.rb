9bd4267f349495b8c85212ef8d8a813984bada04
Raghu Betina <raghu@firstdraft.com>
2018-06-12 01:18:11 +0000

    Whitelist columns in Attestation for ActiveAdmin

--- app/admin/attestations.rb
+++ app/admin/attestations.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Attestation do
+
+ permit_params :physician_id, :ehr_vendor, :ehr_name, :certification_date
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
