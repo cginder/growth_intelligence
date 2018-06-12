a167d69e310f4a7cded807300da6e32f112c2f74
Raghu Betina <raghu@firstdraft.com>
2018-06-12 01:17:59 +0000

    Whitelist columns in Practice for ActiveAdmin

--- app/admin/practices.rb
+++ app/admin/practices.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Practice do
+
+ permit_params :pac_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
